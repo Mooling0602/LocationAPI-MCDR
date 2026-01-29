21544a784f0a86e5bfb5fa0c8fe955875b3cc63f
木之清泠 <clemooling@staringplanet.top>
2026-01-29 21:57:42 +0800

    feat: finish debugging part

--- location_api/mcdr/entry.py
+++ location_api/mcdr/entry.py
@@ -1,7 +1,21 @@
-from mcdreforged.api.all import PluginServerInterface
+from mcdreforged.api.all import PluginServerInterface, Literal, Text
+from location_api.pos import on_debug_pos_help, on_debug_pos
 
 
-def on_load(server: PluginServerInterface, _prev_module):
+async def on_load(server: PluginServerInterface, _prev_module):
+    server.register_command(
+        Literal("!!loc_api").then(
+            Literal("debug").runs(
+                on_debug_pos_help
+            ).then(
+                Literal("pos").then(
+                    Text("player").runs(
+                        on_debug_pos
+                    ).requires(lambda src: src.has_permission_higher_than(2))
+                )
+            )
+        )
+    )
     server.logger.info("Loaded LocationAPI.")
 
 
--- location_api/pos.py
+++ location_api/pos.py
@@ -4,26 +4,28 @@ To use APIs here, you must have `mcdreforged` package installed.
 """
 from returns.result import Success, Failure
 from returns.maybe import Some
-from mcdreforged.api.all import PluginServerInterface
+from mcdreforged.api.all import CommandSource, CommandContext
 from moolings_rcon_api.api import rcon_get_async
 
-_PSI: PluginServerInterface | None = None
+# _PSI: PluginServerInterface | None = None
 
 
-async def get_player_pos(player: str, psi: PluginServerInterface | None = None):
-    if psi:
-        server = psi
-    else:
-        if _PSI:
-            server = _PSI
-        else:
-            raise RuntimeError("PluginServerInterface is not initialized!")
+def on_debug_pos_help(src: CommandSource):
+    src.reply("Usage: !!loc_api debug pos <player>")
+
+
+async def on_debug_pos(src: CommandSource, ctx: CommandContext):
+    result = await get_player_pos(ctx['player'])
+    src.reply(result)
+
+
+async def get_player_pos(player: str):
     query_pos_cmd = f"data get entity {player} Pos"
     query_result = await rcon_get_async(query_pos_cmd)
     match query_result:
         case Success(Some(content)):
-            server.logger.info(f"Get result: {content}")
+            return f"Get result: {content}"
         case Success(_):
-            server.logger.error("Failed to get player position: empty response.")
+            return "Failed to get player position: empty response."
         case Failure(e):
-            server.logger.error(f"Failed to get player position: {e}")
+            return f"Failed to get player position: {e}"
