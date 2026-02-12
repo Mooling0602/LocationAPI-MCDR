9fc06bee361aad36c2391450fae436a2cd4f0e73
木之清泠 <clemooling@staringplanet.top>
2026-02-12 17:21:34 +0800

    release: v0.4.4

--- location_api/api.py
+++ location_api/api.py
@@ -3,7 +3,7 @@
 from location_api import Point2D, Point3D, MCPosition, Location
 from location_api.pos import get_player_pos
 
-__version__ = "0.4.3"
+__version__ = "0.4.4"
 VERSION = __version__
 version = VERSION
 """The version string of LocationAPI plugin.
