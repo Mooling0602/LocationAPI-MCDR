79579b93bc6c4b3e5a73909950019dc12fe49f27
木之清泠 <clemooling@staringplanet.top>
2026-04-10 12:08:59 +0800

    ci(fix): correct docs build tool

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -6,6 +6,6 @@ if [ -n "$LANG_CODE" ]; then
   echo "Building docs for language: $LANG_CODE..."
   sphinx-build -b html source/ "build/html_$LANG_CODE/" -D language="$LANG_CODE"
 else
-  echo "Building docs in English (en_US)..."
+  echo "Building docs in default language..."
   sphinx-build -b html source build/html
 fi
